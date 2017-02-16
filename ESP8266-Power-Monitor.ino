f187a475e6ba1f65b3ea8c2337311624b5387a6f
Jamin <jamminx@gmail.com>
2017-02-17 09:39:05 +1300

    Update ESP8266-Power-Monitor.ino

--- ESP8266-Power-Monitor.ino
+++ ESP8266-Power-Monitor.ino
@@ -328,7 +328,7 @@ void setup() {
 #ifdef USE_LOCAL_SERVER
   Blynk.begin(AUTH, WIFI_SSID, WIFI_PASS, SERVER);
 #else
-  Blynk.begin(AUTH, WIFI_SSID, WIFI_PASS, );
+  Blynk.begin(AUTH, WIFI_SSID, WIFI_PASS);
 #endif
   while (Blynk.connect() == false) {}
 #ifdef OTA_UPDATES
