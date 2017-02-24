974f1ddfbff4cd9b00e2b008c0f7e14458f5bea3
Jamin <jamminx@gmail.com>
2017-02-25 10:44:41 +1300

    fixed compile errors

--- ESP8266-Power-Monitor.ino
+++ ESP8266-Power-Monitor.ino
@@ -293,13 +293,16 @@ void stopwatchCounter() {
   Blynk.virtualWrite(vPIN_ENERGY_TIME, days + hours_o + hours + mins_o + mins + secs_o + secs);
 }
 
-#ifdef FIXED_ENERGY_PRICE
+
 // This section is for setting the kWh price from your electric company.
 // I use a SPOT rate which means I need to update it all the time.
 // If you know your set price per kWh (in cents), then enter the price in settings: FIXED_ENERGY_PRICE
 void getPrice() {
+  #ifdef FIXED_ENERGY_PRICE
   Blynk.virtualWrite(vPIN_ENERGY_API, ENERGY_API); // local API Server to get current power price per mWh
+  #endif
 }
+#ifdef FIXED_ENERGY_PRICE
 BLYNK_WRITE(vPIN_ENERGY_API) {
   energyPrice = param.asFloat();
   Blynk.virtualWrite(vPIN_ENERGY_PRICE, String(energyPrice, 4) + String('c') );
